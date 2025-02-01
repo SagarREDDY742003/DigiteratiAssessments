42328797079e3e144fdf6d717bbbeda207cf563b
SagarREDDY742003 <sagar07042003@gmail.com>
2025-02-01 10:01:27 +0530

    commit

--- Week1/src/oops/ElectricCar.java
+++ Week1/src/oops/ElectricCar.java
@@ -1,10 +1,16 @@
 package oops;
-
-public class ElectricCar {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+import utill.Constants;
+public class ElectricCar extends Car {
+	public String batteryRange;
+	
+	public ElectricCar(String make, String model, int year, String batteryRange) {
+		super(make, model, year);
+		this.batteryRange = batteryRange;
+		
+	}
+	
+	public void chargeBattery() {
+		System.out.println(Constants.CHARGE_BATTERY);
 	}
 
 }
