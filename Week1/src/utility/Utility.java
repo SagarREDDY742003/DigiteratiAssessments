515af3f6d39b2a09d00b97c2a2e6c80bf8280ddc
POTHIREDDY VIDYA SAGAR REDDY <135987806+SagarREDDY742003@users.noreply.github.com>
2025-02-02 15:27:58 +0530

    Delete Week1 directory

--- Week1/src/utility/Utility.java
+++
@@ -1,9 +0,0 @@
-package utility;
-
-public class Utility {
-	// Static method to find the length of a given integer
-	public static int getLength(int number) {
-		return String.valueOf(number).length();
-	}
-
-}
