e30905e7f3013bf180b01d5d14fbad5c4bdf1fc6
SagarREDDY742003 <sagar07042003@gmail.com>
2025-02-15 12:44:55 +0530

    add task1

---
+++ Week3/src/task1/Task4.java
@@ -0,0 +1,13 @@
+package task1;
+
+/*
+Task 4 : Use ArrayCopyRange() function to demonstrate and example such that you copy the range of values in an 
+array. 
+*/
+
+public class Task4 {
+	public static void main(String[] args) {
+//		ArrayList<Integer> arrlst = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6,7,8));
+		
+	}
+}
