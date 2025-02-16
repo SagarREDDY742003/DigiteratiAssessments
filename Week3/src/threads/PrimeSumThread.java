8374aa1b89e32d0d863e926f8dcd76cbfd8b22e1
SagarREDDY742003 <sagar07042003@gmail.com>
2025-02-16 17:40:29 +0530

    add threads

---
+++ Week3/src/threads/PrimeSumThread.java
@@ -0,0 +1,10 @@
+package threads;
+//Calculate the sum of all prime numbers up to a given limit using multiple threads
+public class PrimeSumThread {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
