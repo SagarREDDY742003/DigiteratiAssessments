cf5c05777addc7f48a4b056ebfa1af6eeb4118c8
SagarREDDY742003 <sagar07042003@gmail.com>
2025-03-18 18:41:12 +0530

    add angular

---
+++ Angular/first-angular-app/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { InfoComponent } from './info/info.component';
+import { AttributeExpoComponent } from './attribute-expo/attribute-expo.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InfoComponent,
+    AttributeExpoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Angular/first-angular-app/src/app/info/info.component.ts
@@ -0,0 +1,18 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-info',
+  templateUrl: './info.component.html',
+  styleUrl: './info.component.css'
+})
+export class InfoComponent {
+  @Input("info") information:string=""
+  @Input("aclass") alerttheme=""
+
+  @Output() senseit:EventEmitter<string>=new EventEmitter<string>();
+
+  communicate()
+  {
+     this.senseit.emit(this.information)
+  }
+}
