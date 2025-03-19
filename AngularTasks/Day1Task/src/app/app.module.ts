f9e1ee1d593c887fd5ec685e95e1e65dd3d84db1
SagarREDDY742003 <sagar07042003@gmail.com>
2025-03-19 13:43:39 +0530

    add changes

--- AngularTasks/Day1Task/src/app/app.module.ts
+++ AngularTasks/Day1Task/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskListComponent } from './task-list/task-list.component';
 import { TaskItemComponent } from './task-item/task-item.component';
+import { StudentCardComponent } from './student-card/student-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TaskListComponent,
-    TaskItemComponent
+    TaskItemComponent,
+    StudentCardComponent
   ],
   imports: [
     BrowserModule,
