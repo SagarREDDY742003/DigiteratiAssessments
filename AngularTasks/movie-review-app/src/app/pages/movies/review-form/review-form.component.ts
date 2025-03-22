55fbb0a630c9b29c2b8d34b5cd018c7703aebd96
SagarREDDY742003 <sagar07042003@gmail.com>
2025-03-22 23:50:03 +0530

    add data

--- AngularTasks/movie-review-app/src/app/pages/movies/review-form/review-form.component.ts
+++ AngularTasks/movie-review-app/src/app/pages/movies/review-form/review-form.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ApiService } from '../../../services/api.service';
 
 @Component({
   selector: 'app-review-form',
@@ -6,5 +7,53 @@ import { Component } from '@angular/core';
   styleUrl: './review-form.component.css'
 })
 export class ReviewFormComponent {
+  @Input() moviename:string=""   
+  @Input() category:string=""   
 
+  name: string = '';
+  message: string = '';
+  errors:string[]=[]
+ 
+  movieTypes: string[] = [
+    'All',
+   'Action',
+   'Drama',
+   'Love',
+   'Adventure',
+   'Horror',
+   'Comedy'
+   ]
+ 
+  constructor(private api: ApiService) {}
+  
+  submitForm() {
+    this.errors=[]
+    if(!this.name || this.name.length<3){
+      this.errors.push("Name should be atleast three characters long")
+    }
+    if(!this.message || this.message.length<10){
+      this.errors.push("Message should be atleast 10 characters long")
+    }
+
+    if(this.errors.length==0){
+      this.api.addReview({
+        name: this.name,
+        moviename: this.moviename,
+        category: this.category,
+        message: this.message
+      }).subscribe({
+          next: (response) => {
+            console.log("Review Form Response:", response);
+            this.name = '';
+            this.moviename = '';
+            this.category = '';
+            this.message = '';
+            alert("Review Submitted Successfully")	
+          },
+          error: (error) => {
+            alert("Something went wrong. Please try again")
+         }});
+    }
+ 
+  }
 }
