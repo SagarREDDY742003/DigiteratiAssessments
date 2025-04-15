6178c45e44d3a84a22a035411f49c93984ad7a13
SagarREDDY742003 <sagar07042003@gmail.com>
2025-04-15 10:26:12 +0530

    add

--- FlutterTask/event_finder/lib/createeventscreen.dart
+++ FlutterTask/event_finder/lib/createeventscreen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart'; // For formatting the date
+import 'package:intl/intl.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'localization.dart';
 
